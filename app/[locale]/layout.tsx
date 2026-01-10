7035ffd1025758138b8930328f6ea26e8ab30da6
AldyRahmatY <aldyrahmaty@gmail.com>
2026-01-10 11:26:23 +0700

    feat: add VisitorTracker component to RootLayout

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -38,6 +38,7 @@ export default async function RootLayout({
             {children}
           </main>
           <Footer />
+          <VisitorTracker />
         </NextIntlClientProvider>
       </body>
     </html>
